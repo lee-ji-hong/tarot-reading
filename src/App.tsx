f5543cf6f184446c6bc0568d9130722561b5c456
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-01-09 02:02:25 +0900

    Feat: "배경이미지 테스트"

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useState } from 'react';
-import { useQuery } from 'react-query';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
 
 import { CallGPT } from './api/gpt'
 import classNames from 'classnames/bind'
@@ -9,13 +9,14 @@ import styles from './App.modules.scss'
 const cx = classNames.bind(styles)
 
 function App() {
-  const endpoint = '/chat/completions';
-  const { isLoading, error, data } = useQuery<string>('posts', () => CallGPT(endpoint))
-  if (isLoading) return <div>Loading...</div>;
+  const endpoint = '/chat/completions'
+  // const { isLoading, error, data } = use Query<string>('posts', () => CallGPT(endpoint))
+  // if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className={cx('container')}>
-      {data}
+      {/* {data} */}
+      안녕하세요! 무엇을 도와드릴까요?
     </div>
   )
 }
