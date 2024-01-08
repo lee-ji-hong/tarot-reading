aa37975559878a5acd9a301169e1913defb6d672
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-01-08 18:16:26 +0900

    Feat:"react-query 연동"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
 import { CallGPT } from './api/gpt'
 import classNames from 'classnames/bind'
 import styles from './App.modules.scss'
 
 const cx = classNames.bind(styles)
+
 function App() {
-  const handleClickAPICall = async () => {
-    await CallGPT()
-  }
+  const endpoint = '/chat/completions';
+  const { isLoading, error, data } = useQuery<string>('posts', () => CallGPT(endpoint))
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className={cx('container')}>
-      <button onClick={handleClickAPICall}>클릭</button>
+      {data}
     </div>
   )
 }
--- src/api/gpt.ts
+++ src/api/gpt.ts
@@ -1,28 +1,30 @@
 
-// const api_url = process.env.REACT_APP_GPT_API_KEY as string;
+const BASE_URL = process.env.REACT_APP_GPT_API_URL as string;
 
-export const CallGPT = async () => {
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
+export const CallGPT = async (endpoint: string): Promise<string> => {
+  const response = await fetch(BASE_URL + endpoint, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.REACT_APP_GPT_API_KEY}`,
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: 'gpt-3.5-turbo',
       messages: [
         {
-          "role": "system",
-          "content": "You are a helpful assistant."
-        }
+          role: 'system',
+          content: '안녕',
+        },
       ],
-      temperature: 0.7,
-      max_token: 1_000,
-    })
-  })
-  const responseData = await response.json()
-  console.log("=>응답값은 " + responseData)
+    }),
+  });
 
-  return responseData
-}
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
 
+  const responseData = await response.json();
+  const message = responseData.choices[0].message.content;
+  console.log(message)
+  return message;
+};
