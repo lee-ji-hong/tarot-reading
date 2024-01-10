a8f0147dc9d4257c4bd30460e24817d13ace1914
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-01-10 16:38:03 +0900

    Feat:"카드 뒤집기 기능 구현"

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -11,19 +11,35 @@ const cx = classNames.bind(styles)
 
 function ResultPage() {
   const data = useRecoilValue(LuckData);
-
   const getImagePath = (index: number) => `/assets/images/card${index + 1}.png`;
+  const cardsData = data?.cards;
+  const [clickedCards, setClickedCards] = useState(Array(cardsData.length).fill(false));
+
+  const handleCardClick = (index: number) => {
+    const updatedClickedCards = [...clickedCards];
+    updatedClickedCards[index] = !updatedClickedCards[index];
+    setClickedCards(updatedClickedCards);
+  };
+
   console.log(data)
 
   return (
     <div className={cx('container')}>
-      {[...Array(3)].map((_, idx) => (
+      {cardsData.map((card, idx) => (
         <div
           key={idx}
           className={cx('wrap-image')}
-          onClick={() => alert('클릭')}
+          onClick={() => handleCardClick(idx)}
         >
-          <img src={getImagePath(idx)} alt={`card ${idx + 1}`} />
+          <div className={cx('card', { active: clickedCards[idx] })}>
+            <div className={cx('front')}>
+              <img src={card?.cardImageUrl} alt={`card ${idx + 1}`} />
+            </div>
+
+            <div className={cx('back')}>
+              <img src={getImagePath(idx)} alt={`card ${idx + 1}`} />
+            </div>
+          </div>
         </div>
       ))}
     </div>
